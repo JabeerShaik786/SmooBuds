adbac745b09e05a9ef8c83d684e3cd86cc9978bc
JabeerShaik786 <23p31a4497@acet.ac.in>
2026-06-30 18:43:22 +0000

    Deploying to gh-pages from @ JabeerShaik786/SmooBuds@82260af4a24e1d010a0ef5762456fb2c27ebfae1 🚀

--- _next/static/NEvhQp3RFyMvLZ3bTc8eH/_buildManifest.js
+++ _next/static/NEvhQp3RFyMvLZ3bTc8eH/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/SmooBuds//_next/:path+",
+        "destination": "/SmooBuds/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
